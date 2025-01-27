e3787a316f3a365aec55b5b4f42e912481fdb83d
Vitor Paixão <paixaoadm@outlook.com>
2025-01-26 23:15:29 -0400

    Novas funcionalidades e correcoes

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 let listaDeAmigos = [];
+let amigosSorteados = [];
 
 function adicionarAmigo() {
     let amigo = document.getElementById("amigo").value.trim();
 
     if (amigo === '') {
         alert('Digite o nome do amigo!');
+        return;
+    }
+
+    amigo = amigo.toLowerCase();
+
+    if (listaDeAmigos.includes(amigo)) {
+        alert('Este nome já foi adicionado!');
         return;
     }
 
@@ -19,8 +27,27 @@ function adicionarAmigo() {
 
 function sortearAmigo() {
     if (listaDeAmigos.length > 0) {
-        let amigoSorteado = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
-        document.getElementById('resultado').innerHTML = amigoSorteado;
+        if (amigosSorteados.length === listaDeAmigos.length) {
+            document.getElementById('resultado').innerHTML = 'Todos os amigos já foram sorteados!';
+            return;
+        }
+
+        let amigoSorteado;
+        do { 
+            amigoSorteado = listaDeAmigos[Math.floor(Math.random() * listaDeAmigos.length)];
+        } while (amigosSorteados.includes(amigoSorteado));
+
+        amigosSorteados.push(amigoSorteado);
+        let resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.innerHTML = `<p>${capitalizar(amigoSorteado)}</p>`;
+
+        let hideButton = document.createElement('button');
+        hideButton.innerText = 'Ocultar Resultado';
+        hideButton.classList.add('hide-button');
+        hideButton.onclick = function() {
+            resultadoDiv.innerHTML = '';
+        };
+        resultadoDiv.appendChild(hideButton);
     } else {
         document.getElementById('resultado').innerHTML = 'Adicione amigos para sortear!';
     }
@@ -31,9 +58,20 @@ function atualizarListaDeAmigos() {
     lista.innerHTML = '';
     listaDeAmigos.forEach((amigo) => {
         let item = document.createElement('li');
-        item.innerHTML = amigo;
+        item.innerHTML = capitalizar(amigo);
         lista.appendChild(item);
     });
+}
+
+function limparLista() {
+    listaDeAmigos = [];
+    amigosSorteados = [];
+    atualizarListaDeAmigos();
+    document.getElementById('resultado').innerHTML = 'A lista foi limpa!';
+}
+
+function capitalizar(nome) {
+    return nome.charAt(0).toUpperCase() + nome.slice(1);
 }
 
 let input = document.querySelector("input");
